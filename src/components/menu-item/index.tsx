32120f15418333ff158cef1dd3101a07e99a7dc8
Matt Lucock <2455682+matthewlucock@users.noreply.github.com>
2021-02-15 10:31:10 +1100

    Implement new menu

---
+++ src/components/menu-item/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import clsx from 'clsx'
+import type { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import styles from './styles.scss'
+
+type Props = Readonly<{
+  className?: string
+  icon?: IconProp
+  onClick?: () => void
+}>
+
+export const MenuItem: React.FC<Props> = props => (
+  <div className={clsx(styles.menuItem, props.className)} onClick={props.onClick}>
+    {props.icon && (
+      <FontAwesomeIcon className={styles.icon} icon={props.icon} />
+    )}
+    {props.children}
+  </div>
+)
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,14 +1,34 @@
 import * as React from 'react'
 import clsx from 'clsx'
+import type { Padding as PopperPadding } from '@popperjs/core'
+import { usePopper } from 'react-popper'
 
 import styles from './styles.scss'
 
 type Props = Readonly<{
+  button: HTMLDivElement | null
+  offset?: PopperPadding
   visible: boolean
 }>
 
-export const Menu: React.FC<Props> = props => (
-  <div className={clsx(styles.container, props.visible && styles.visible)}>
-    {props.children}
-  </div>
-)
+export const Menu: React.FC<Props> = props => {
+  const [menu, setMenu] = React.useState<HTMLDivElement | null>(null)
+  const [arrow, setArrow] = React.useState<HTMLDivElement | null>(null)
+  const { styles: popperStyles } = usePopper(props.button, menu, {
+    modifiers: [
+      { name: 'arrow', options: { element: arrow } },
+      { name: 'preventOverflow', options: { padding: props.offset } }
+    ]
+  })
+
+  return (
+    <div
+      ref={setMenu}
+      className={clsx(styles.menu, props.visible && styles.visible)}
+      style={popperStyles.popper}
+    >
+      <div ref={setArrow} className={styles.arrow} style={popperStyles.arrow} />
+      {props.children}
+    </div>
+  )
+}
